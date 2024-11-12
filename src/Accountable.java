07dd4bc407360f3645876b2b32ed03ed5f44e0d7
DAlbardi <davide.albani01@universitadipavia.it>
2024-11-12 14:13:51 +0100

    Alcuni commenti

---
+++ src/Accountable.java
@@ -0,0 +1,11 @@
+public class Accountable {
+    private String nome;
+    private double quantita;
+    private String data;
+
+    public Accountable(String nome, double quantita, String data) {
+        this.nome = nome;
+        this.quantita = quantita;
+        this.data = data;
+    }
+}
